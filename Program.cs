b245d8917916b3fcfa758b39634f01dd5afe245e
Alan M.E <alanmarquez@outlook.com>
2018-12-23 20:27:01 -0500

    added main

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppContext appContext = AppContext.GetIntance();
+
+            appContext.SetAttribute("nameApp", "Tank!");
+            appContext.SetAttribute("version", new Version("1.0.0"));
+
+            appContext.AddEvent("onAlert", (eventArgs) =>
+            {
+                Console.WriteLine($@"Name: { appContext.GetAttribute("nameApp") }, Version: { appContext.GetAttribute("version") }");
+            });
+
+            appContext.EmitEvent("onAlert");
+            Console.ReadKey();
+        }
+    }
+}
