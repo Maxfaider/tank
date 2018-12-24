cb3340f911acf590a4d133bdb1e12be58fd71ea9
Alan M.E <alanmarquez@outlook.com>
2018-12-23 19:44:15 -0500

    added EventsListener

---
+++ EventsListener.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank
+{
+
+    public delegate void GenericEventHandler(GenericEventArgs eventArgs);
+
+    public class EventsListener
+    {
+        Dictionary<string, GenericEventHandler> events;
+        string nameListener;
+        string dateCreated;
+
+        public static EventsListener newEventsListener(string name) => new EventsListener(name);
+
+        private EventsListener(string name)
+        {
+            this.nameListener = name;
+            this.dateCreated = DateTime.Now.ToString("f");
+        }
+
+        public void RegisterEvents(string name, GenericEventHandler callback)
+        {
+            events.Add(name, callback);
+        }
+
+        public GenericEventHandler GetEvent(string name)
+        {
+            return events[name];
+        }
+
+        public string NameListener
+        {
+            get { return nameListener; }
+        }
+
+        public string DateCreated
+        {
+            get { return dateCreated; }
+        }
+
+    }
+}
