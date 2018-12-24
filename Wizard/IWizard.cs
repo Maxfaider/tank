7a7986893eab89c99a952c225dd3d81bfca51731
Alan M.E <alanmarquez@outlook.com>
2018-12-23 19:15:12 -0500

    added Wizard features

---
+++ Wizard/IWizard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank.Wizard
+{
+    interface IWizard
+    {
+        int WizardValidate();
+        bool WizardBusy();
+    }
+}
