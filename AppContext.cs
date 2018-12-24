79b4da75f6c38facf18f9fa8e387deaf3e16de46
Alan M.E <alanmarquez@outlook.com>
2018-12-23 19:16:35 -0500

    added global attributes

---
+++ AppContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank
+{
+    class AppContext
+    {
+        private static readonly AppContext appContext = new AppContext();
+
+        public Dictionary<string, object> attributes;
+       
+        public static AppContext GetIntance()
+        {
+            return appContext;
+        }
+
+        public AppContext()
+        {}
+
+        public void SetAttribute(string name, object value) => attributes.Add(name, value);
+
+        public object GetAttribute(string name) => attributes[name];
+
+    }
+}
